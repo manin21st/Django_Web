866ad75626b8daa26ff47124434394e6772eb8c8
manin21st <manin21st@gmail.com>
2019-01-28 19:55:32 +0900

    Django admin table creation, python3.7-django1.11-cx_oracle6.4.1

--- eis/views.py
+++ eis/views.py
@@ -6,3 +6,19 @@ from .serializers import SalesPvsrCymSerializer
 class SalesPvsrCymViewSet(viewsets.ModelViewSet):
     queryset = SalesPvsrCym.objects.all()
     serializer_class = SalesPvsrCymSerializer
+
+    # HTTP GET /blog/post/
+    # HTTP GET /blog/post/?search=
+    def get_queryset(self):
+        qs = super().get_queryset()
+
+        search = self.request.query_params.get('search','')
+        if search:
+            qs = qs.filter(yymm__icontains=search)
+
+        return qs
+
+    # # HTTP GET /blog/post/get_django/
+    # @list_route()
+    # def get_django(self, request):
+    #     qs = self.get_queryset().filter(yymm__icontains='20')
